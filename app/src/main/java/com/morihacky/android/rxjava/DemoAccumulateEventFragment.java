b26a22728a85345616e53faddc3650da05483495
Kaushik Gopal <code@kaush.co>
2014-07-22 23:40:16 -0700

    feat: wiring up button tap with logs

--- app/src/main/java/com/morihacky/android/rxjava/DemoAccumulateEventFragment.java
+++ app/src/main/java/com/morihacky/android/rxjava/DemoAccumulateEventFragment.java
@@ -1,18 +1,31 @@
 package com.morihacky.android.rxjava;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.morihacky.android.rxjava.app.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class DemoAccumulateEventFragment
     extends Fragment {
+
+    @InjectView(R.id.accumulated_event_list) ListView _logsListView;
+
+    private LogAdapter _adapter;
+    private List<String> _logs;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -21,5 +34,40 @@ public class DemoAccumulateEventFragment
         View layout = inflater.inflate(R.layout.fragment_accumulate, container, false);
         ButterKnife.inject(this, layout);
         return layout;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        _setupLogAdapter();
+    }
+
+    @OnClick(R.id.accumulated_event_btn)
+    public void onButtonTapped() {
+
+        _addLogToAdapter("Tapped");
+    }
+
+    // -----------------------------------------------------------------------------------
+    // Method that help wiring up the example (irrelevant to RxJava)
+
+    private void _setupLogAdapter() {
+        _logs = new ArrayList<String>();
+        _adapter = new LogAdapter(getActivity(), new ArrayList<String>());
+        _logsListView.setAdapter(_adapter);
+    }
+
+    private void _addLogToAdapter(String logMsg) {
+        _logs.add(0, logMsg);
+        _adapter.clear();
+        _adapter.addAll(_logs);
+    }
+
+    private class LogAdapter
+        extends ArrayAdapter<String> {
+
+        public LogAdapter(Context context, List<String> logs) {
+            super(context, R.layout.item_log, R.id.item_log, logs);
+        }
     }
 }
