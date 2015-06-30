89af9779c0c02811bf9a67c0681629257a37bc0c
Kaushik Gopal <code@kaush.co>
2015-06-30 15:14:58 -0700

    feat: add my RxUtils class with CompositeSubscription helpers

---
+++ app/src/main/java/com/morihacky/android/rxjava/RxUtils.java
@@ -0,0 +1,21 @@
+package com.morihacky.android.rxjava;
+
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
+
+public class RxUtils {
+
+    public static void unsubscribeIfNotNull(Subscription subscription) {
+        if (subscription != null) {
+            subscription.unsubscribe();
+        }
+    }
+
+    public static CompositeSubscription getNewCompositeSubIfUnsubscribed(CompositeSubscription subscription) {
+        if (subscription == null || subscription.isUnsubscribed()) {
+            return new CompositeSubscription();
+        }
+
+        return subscription;
+    }
+}
