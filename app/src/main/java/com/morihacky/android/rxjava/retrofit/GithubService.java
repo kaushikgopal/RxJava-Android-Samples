a89840c1954f2f701bdc16a64033882abba4c18f
Mitchell Tilbrook <marukami.kaito@gmail.com>
2016-04-15 16:30:56 +1000

    upgrade: retrofit 2.0.0-beta3 -> 2.0.0

--- app/src/main/java/com/morihacky/android/rxjava/retrofit/GithubService.java
+++ app/src/main/java/com/morihacky/android/rxjava/retrofit/GithubService.java
@@ -8,9 +8,9 @@ import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import retrofit2.GsonConverterFactory;
 import retrofit2.Retrofit;
-import retrofit2.RxJavaCallAdapterFactory;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import static java.lang.String.format;
 
