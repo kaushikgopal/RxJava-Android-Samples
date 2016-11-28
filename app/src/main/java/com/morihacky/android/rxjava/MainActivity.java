d83a66659511df8d977db6f11c4cac3b01707397
Kaushik Gopal <code@kaush.co>
2016-11-27 17:32:32 -0800

    feat: port timeout + playground
    * port example Timeout
    * port PlaygroundFragment

--- app/src/main/java/com/morihacky/android/rxjava/MainActivity.java
+++ app/src/main/java/com/morihacky/android/rxjava/MainActivity.java
@@ -3,24 +3,15 @@ package com.morihacky.android.rxjava;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import com.morihacky.android.rxjava.fragments.MainFragment;
 import com.morihacky.android.rxjava.fragments.RotationPersist1WorkerFragment;
 import com.morihacky.android.rxjava.fragments.RotationPersist2WorkerFragment;
+import com.morihacky.android.rxjava.fragments.TimeoutDemoFragment;
 import com.morihacky.android.rxjava.rxbus.RxBus;
 
 public class MainActivity
       extends FragmentActivity {
 
     private RxBus _rxBus = null;
-
-    // This is better done with a DI Library like Dagger
-    public RxBus getRxBusSingleton() {
-        if (_rxBus == null) {
-            _rxBus = new RxBus();
-        }
-
-        return _rxBus;
-    }
 
     @Override
     public void onBackPressed() {
@@ -34,24 +25,38 @@ public class MainActivity
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                  .replace(android.R.id.content, new MainFragment(), this.toString())
-                  .commit();
+                                       //.replace(android.R.id.content, new MainFragment(), this.toString())
+                                       .replace(android.R.id.content, new TimeoutDemoFragment(), this.toString())
+                                       .commit();
         }
     }
 
-    private void _removeWorkerFragments() {
-        Fragment frag = getSupportFragmentManager()//
-              .findFragmentByTag(RotationPersist1WorkerFragment.class.getName());
-
-        if (frag != null) {
-            getSupportFragmentManager().beginTransaction().remove(frag).commit();
+    // This is better done with a DI Library like Dagger
+    public RxBus getRxBusSingleton() {
+        if (_rxBus == null) {
+            _rxBus = new RxBus();
         }
 
-        frag = getSupportFragmentManager()//
-              .findFragmentByTag(RotationPersist2WorkerFragment.class.getName());
+        return _rxBus;
+    }
+
+    private void _removeWorkerFragments() {
+        Fragment frag = getSupportFragmentManager().findFragmentByTag(RotationPersist1WorkerFragment.class.getName());
 
         if (frag != null) {
-            getSupportFragmentManager().beginTransaction().remove(frag).commit();
+            getSupportFragmentManager()
+                  .beginTransaction()
+                  .remove(frag)
+                  .commit();
+        }
+
+        frag = getSupportFragmentManager().findFragmentByTag(RotationPersist2WorkerFragment.class.getName());
+
+        if (frag != null) {
+            getSupportFragmentManager()
+                  .beginTransaction()
+                  .remove(frag)
+                  .commit();
         }
     }
 }
