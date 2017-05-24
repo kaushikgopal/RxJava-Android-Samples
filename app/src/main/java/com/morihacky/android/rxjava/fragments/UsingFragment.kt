ea5877bd6d58409ba6a79ccc5fd85b139b70dca3
Kaushik Gopal <code@kaush.co>
2017-05-24 15:17:31 -0700

    --wip--

---
+++ app/src/main/java/com/morihacky/android/rxjava/fragments/UsingFragment.kt
@@ -0,0 +1,17 @@
+package com.morihacky.android.rxjava.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class UsingFragment : BaseFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
