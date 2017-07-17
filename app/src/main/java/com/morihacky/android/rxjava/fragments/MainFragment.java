e83b02e1cf3b068f4095c90c5ff978aaa3b7e1a3
Kaushik Gopal <code@kaush.co>
2017-07-17 10:40:52 -0700

    fix: update rotation persist changes

--- app/src/main/java/com/morihacky/android/rxjava/fragments/MainFragment.java
+++ app/src/main/java/com/morihacky/android/rxjava/fragments/MainFragment.java
@@ -96,8 +96,9 @@ public class MainFragment extends BaseFragment {
 
   @OnClick(R.id.btn_demo_rotation_persist)
   void demoRotationPersist() {
-    clickedOn(new RotationPersist2Fragment());
-    //clickedOn(new RotationPersist1Fragment());
+    clickedOn(new RotationPersist3Fragment());
+    // clickedOn(new RotationPersist2Fragment());
+    // clickedOn(new RotationPersist1Fragment());
   }
 
   @OnClick(R.id.btn_demo_pagination)
